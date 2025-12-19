aae543e93a2b05fd9013f793a32949fbdd9f33aa
mukulmj <mukul.joshi@opstree.com>
2025-12-20 01:22:59 +0530

    visual presentable

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'mukul-site-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './projects.html',
+  './resume.html',
+  './resume-preview.html',
+  './styles.css',
+  './favicon-16.png',
+  './favicon-32.png',
+  './favicon-48.png',
+  './favicon-64.png',
+  './apple-touch-icon.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
