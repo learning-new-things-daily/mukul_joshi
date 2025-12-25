b92b21375010588f8aa4d06ae108edd18075d9b5
mukulmj <mukul.joshi@opstree.com>
2025-12-25 13:06:56 +0530

    updated home path

--- portfolio-react/src/main.jsx
+++ portfolio-react/src/main.jsx
@@ -7,7 +7,7 @@ import { initWebVitals } from './utils/webVitals.js'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
