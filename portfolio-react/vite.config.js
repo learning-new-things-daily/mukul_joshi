3cb0d041d6696c73093405a20d0d8cf587512778
Mukul Joshi <mukuljoshi50@gmail.com>
2026-07-07 17:17:36 +0530

    Update vite.config.js

--- portfolio-react/vite.config.js
+++ portfolio-react/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: { port: 5173 },
-  base: mode === 'production' ? '/mukul_joshi/' : '/',
+  base: mode === 'production' ? '/' : '/',
   test: {
     environment: 'jsdom',
     setupFiles: ['src/setupTests.js'],
